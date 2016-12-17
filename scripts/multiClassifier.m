211c5820508524128d96daf6226da58632cd756b
amandavinci@gmail.com <amanthevinci@gmail.com>
2016-12-17 21:03:41 +0530

    Added main module

--- scripts/multiClassifier.m
+++ scripts/multiClassifier.m
@@ -1,21 +1,3 @@
-%% Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
-
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     lrCostFunction.m (logistic regression cost function)
-%     oneVsAll.m
-%     predictOneVsAll.m
-%     predict.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-
 %% Initialization
 clear ; close all; clc
 
@@ -32,7 +14,7 @@ num_labels = 10;          % 10 labels, from 1 to 10
 % Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
 
-load('ex3data1.mat'); % training data stored in arrays X, y
+load('../data/ex3data1.mat'); % training data stored in arrays X, y
 m = size(X, 1);
 
 % Randomly select 100 data points to display
